90b49fb4f20605a51ec85a2c4de1b6d04c00b6f8
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:04:11 +0000

    docs: add JSDoc comments to hashcode.ts

--- src/hashcode.ts
+++ src/hashcode.ts
@@ -1,6 +1,10 @@
 
 // Type exports for consumers
 // Add specific types as needed
+/**
+ * hashCode
+ * @param str
+ */
 export function hashCode(str: string): number {
   let hash = 0;
   for (let i = 0; i < str.length; i++) hash = ((hash << 5) - hash + str.charCodeAt(i)) | 0;
